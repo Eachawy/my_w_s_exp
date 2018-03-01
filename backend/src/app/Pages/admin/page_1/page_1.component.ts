9bfeb854e21c09421103daee4f0cb9f7a880d421
muhammad Abdul monem <muhammad.monem@intellisc.com>
2018-03-01 13:07:25 +0200

    update

---
+++ backend/src/app/Pages/admin/page_1/page_1.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'page1',
+    templateUrl:'./page_1.component.html',
+    styleUrls:['./page_1.component.scss']
+})
+
+export class page_1Component {}
